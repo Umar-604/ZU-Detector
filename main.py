20f14fc39f2264d2cbb29c53311dbe182ff5fca9
Umar <umarr.k220@gmail.com>
2025-02-10 09:24:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ import pyfiglet
 def run_PE():
     file = input("Enter the path and name of the file : ")
     os.system("python3 Extract/PE_main.py {}".format(file))
+
+def run_URL():
+    os.system('python3 Extract/url_main.py')
+
+def exit():
+    os.system('exit')
