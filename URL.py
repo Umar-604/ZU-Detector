2d60b5a66fcd50ad914908fe28365883f4ccf952
Umar <umarr.k220@gmail.com>
2025-03-03 22:20:46 +0500

    Update URL.py

--- URL.py
+++ URL.py
@@ -3,3 +3,6 @@ import pickle
 # Take URL input
 urls = []
 urls.append(input("Input the URL that you want to check (e.g., google.com): "))
+
+# Define a whitelist of trusted domains
+whitelist = ['hackthebox.eu', 'root-me.org', 'gmail.com']
