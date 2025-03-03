3ba17636ef5593b004340142f872540f8ea1e05c
Muhammad Zeeshan Khan <muhammadzeeshan123345@gmail.com>
2025-03-03 22:33:02 +0500

    Update PE_main.py

--- PE_main.py
+++ PE_main.py
@@ -23,3 +23,25 @@ def get_entropy(data):
             entropy -= p_x * math.log(p_x, 2)
 
     return entropy
+
+
+# For extracting the resources part
+def get_resources(pe):
+    """Extract resources:
+    [entropy, size]"""
+    resources = []
+    if hasattr(pe, 'DIRECTORY_ENTRY_RESOURCE'):
+        try:
+            for resource_type in pe.DIRECTORY_ENTRY_RESOURCE.entries:
+                if hasattr(resource_type, 'directory'):
+                    for resource_id in resource_type.directory.entries:
+                        if hasattr(resource_id, 'directory'):
+                            for resource_lang in resource_id.directory.entries:
+                                data = pe.get_data(resource_lang.data.struct.OffsetToData, resource_lang.data.struct.Size)
+                                size = resource_lang.data.struct.Size
+                                entropy = get_entropy(data)
+
+                                resources.append([entropy, size])
+        except Exception as e:
+            return resources
+    return resources
