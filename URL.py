21b187e8ff51ea9743900ffff5ccb5f0487f7813
Umar <umarr.k220@gmail.com>
2025-03-06 23:23:49 +0500

    Update URL.py

--- URL.py
+++ URL.py
@@ -6,3 +6,12 @@ urls.append(input("Input the URL that you want to check (e.g., google.com): "))
 
 # Define a whitelist of trusted domains
 whitelist = ['hackthebox.eu', 'root-me.org', 'gmail.com']
+
+# Sanitization function for processing URLs
+def improved_sanitization(url):
+    url = str(url).lower()  # Ensure input is a string and lowercase it
+    url = url.split('://')[-1]  # Remove protocol
+    if url.startswith('www.'):
+        url = url[4:]  # Remove 'www.'
+    parts = url.split('/')
+    tokens = []
